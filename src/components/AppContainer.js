a1c5fcaa209d4d523fa5993cee2f8ead6d30e7fc
Anna <nutkatuz@yandex.ru>
2020-11-08 20:06:09 +0500

    yandex react app 11

---
+++ src/components/AppContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AppContainer.css';
+import FoodAdder  from './FoodAdder'
+
+function AppContainer() {
+  return (
+    <div className="container">
+      <div className="calories">
+        <h2>Калории:</h2>
+      </div>
+   <FoodAdder />
+    </div>
+  );
+}
+
+export default AppContainer;
---
+++ src/components/FoodAdder.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './FoodAdder.css';
+
+class FoodAdder extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        FoodList: []
+      }
+    }
+  
+    handleSubmit = () => {
+    }
+  
+    render(){
+    return (
+      <form className="food-adder">
+        <input type="text" placeholder="Добавьте продукты" />
+        <button className="food-adder__submit"></button>
+      </form>
+    )}
+  }
+  
+  export default FoodAdder;
